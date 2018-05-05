bae58b1db7f02707bb5dfb0f6e735c5ccdce161f
= <proditaki@gmail.com>
2018-05-05 02:43:52 +0200

    test

--- app.js
+++ app.js
@@ -40,53 +40,52 @@ class GoTF {
         th2.innerHTML = 'Messages'		
 
         tr.appendChild(th)
-		tr.appendChild(th2)
-		
-		table.appendChild(tr)
-		table.appendChild(tr2)
-		
-		var users = this.data.dates[headingText]		
-		
-		var sorted = [];
-		
-		for (var user in users) {
-			sorted.push([user, users[user]]);
-		}
-		
-		sorted.sort(function(a,b) {
-			return b[1] - a[1];
-		});
-		
-		var i
-		
-		for (i = 0 ; i < sorted.length ; i++)
-		{
-			const l_tr = document.createElement('tr')
-			const td = document.createElement('td')
-			const td2 = document.createElement('td')
-			
-			td.innerHTML = this.data.users[sorted[i][0]]
-			td2.innerHTML =  sorted[i][1]
-			l_tr.appendChild(td)
-			l_tr.appendChild(td2)
-			table.appendChild(l_tr)
-		}
-		
-	/*	for (var user in users) {
-			console.log(user)
-			console.log(sorted[user])
-			const l_tr = document.createElement('tr')
-			const td = document.createElement('td')
-			const td2 = document.createElement('td')
-			td.innerHTML = this.data.users[user]
-			td2.innerHTML = users[user]
-			l_tr.appendChild(td)
-			l_tr.appendChild(td2)
-			table.appendChild(l_tr)
+	tr.appendChild(th2)
 
+	table.appendChild(tr)
+	table.appendChild(tr2)
+
+	var users = this.data.dates[headingText]		
+
+	var sorted = [];
+
+	for (var user in users) {
+		sorted.push([user, users[user]]);
+	}
+
+	sorted.sort(function(a,b) {
+		return b[1] - a[1];
+	});
+
+	var i
+
+	for (i = 0 ; i < sorted.length ; i++)
+	{
+		const l_tr = document.createElement('tr')
+		const td = document.createElement('td')
+		const td2 = document.createElement('td')
+
+		td.innerHTML = this.data.users[sorted[i][0]]
+		td2.innerHTML =  sorted[i][1]
+		l_tr.appendChild(td)
+		l_tr.appendChild(td2)
+		table.appendChild(l_tr)
+	}
+
+/*	for (var user in users) {
+		console.log(user)
+		console.log(sorted[user])
+		const l_tr = document.createElement('tr')
+		const td = document.createElement('td')
+		const td2 = document.createElement('td')
+		td.innerHTML = this.data.users[user]
+		td2.innerHTML = users[user]
+		l_tr.appendChild(td)
+		l_tr.appendChild(td2)
+		table.appendChild(l_tr)
 			//sorted.push([user, users[user]]);
-		}*/
-		
+	}*/
+
 
 
         this.main.appendChild(table)
